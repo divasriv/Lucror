9c40d1fccf09ab9f772bc0bb293c1bc47a817ba3
Diva <divasriv02@gmail.com>
2022-12-31 16:30:34 +0530

    modified home page for mobile

--- src/index.js
+++ src/index.js
@@ -65,14 +65,14 @@ import GoogleMap from "./blocks/GoogleMap";
 import Columns from "./blocks/Columns";
 import PricingTable from "./blocks/PricingTable";
 
-import { HashRouter, Switch, Route, } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 
 class Root extends Component{
     render(){
         return(
-            <HashRouter basename={process.env.PUBLIC_URL}>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Switch>
                     <Route exact path={`/`} component={Demo}/>
                     <Route exact path={`/main-demo`} component={MainDemo}/>
@@ -132,7 +132,7 @@ class Root extends Component{
                     <Route component={error404}/>
 
                 </Switch>
-            </HashRouter>
+            </BrowserRouter>
         )
     }
 }
